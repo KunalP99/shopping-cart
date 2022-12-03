2f04bd0237dd3944e16761173b0be70e962d524f
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-12-03 22:41:33 +0000

    Styled sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import Close from "../images/close.svg";
+import Minus from "../images/minus.svg";
+import Plus from "../images/plus.svg";
 
 export default function Sidebar(props) {
   const closeSidebar = () => {
     const sidebar = document.querySelector(".sidebar-container");
     sidebar.classList.remove("show");
   };
-  console.log(props.basket);
 
   return (
     <div className='sidebar-container'>
@@ -18,10 +19,26 @@ export default function Sidebar(props) {
       <div className='checkout-container'>
         <h2>CHECKOUT</h2>
         {typeof props.basket !== "undefined" &&
-          props.basket.map((item) => {
+          props.basket.map((item, index) => {
             return (
-              <div key={`checkout-`}>
-                <p>{item.name}</p>
+              <div
+                className={`checkout-item-${index} checkout-item`}
+                key={index}
+              >
+                <div className='name-price-container'>
+                  <p>{item.name}</p>
+                  <p
+                    className={`checkout-item-${index}-price`}
+                  >{`£${item.price}`}</p>
+                </div>
+                <div className='quantity-remove-container'>
+                  <div className='quantity-container'>
+                    <img src={Minus} alt='Minus quantity' />
+                    <p>1</p>
+                    <img src={Plus} alt='Add quantity' />
+                  </div>
+                  <button>Remove</button>
+                </div>
               </div>
             );
           })}
